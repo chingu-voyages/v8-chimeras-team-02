5a54cb2539f66ea159d5c3f2bb50ee8fe047c7fd
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-21 10:01:38 +0100

    create not found page component

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => (
+  <div style={{ textAlign: "center", background: "white" }}>
+    <h1>Not Found!!1!</h1>
+  </div>
+);
+
+export default NotFound;
