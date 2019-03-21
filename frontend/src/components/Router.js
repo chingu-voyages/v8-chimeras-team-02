07147c70f2312b800548611ec4fa0a47f474bebd
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-21 10:07:26 +0100

    move notfound to screens and update imports

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../App";
-import NotFound from "./NotFound";
+import NotFound from "../screens/NotFound";
 
 const Router = () => (
   <BrowserRouter>
