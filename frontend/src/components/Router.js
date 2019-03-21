4c0642dce4eed242604a3984221803be79c3f814
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-21 10:02:04 +0100

    add not found route

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../App";
+import NotFound from "./NotFound";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={App} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
