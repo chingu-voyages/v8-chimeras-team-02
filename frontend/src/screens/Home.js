407f1c583dc4e88c67c8fc7a75515830ea0ba92e
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-21 10:45:26 +0100

    prettier format on home screen

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,80 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { user } from "../resources/images";
 import { blue } from "../resources/colors";
-import {
-    SearchBar,
-    Section,
-    ListItem,
-    Logo,
-    SideList
-} from '../components';
+import { SearchBar, Section, ListItem, Logo, SideList } from "../components";
 
 export default class Home extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div style={container}>
+        <div style={header}>
+          <Logo />
+          <SearchBar />
+          <Section title="Ask" />
+          <Section title="Login" />
+          <Section title="Signup" />
 
-            <div style={container}>
-                <div style={header}>
-                    <Logo />
-                    <SearchBar />
-                    <Section title="Ask" />
-                    <Section title="Login" />
-                    <Section title="Signup" />
+          <img src={user} style={avatar} alt={user} />
+        </div>
 
-                    <img src={user} style={avatar} alt={user} />
-                </div>
-
-                <div style={gridView}>
-                    <SideList />
-                    <div style={listview}>
-                        <ListItem
-                            title={"Undefined is not an object React Native"}
-                            user={"Hanen Wahabi"}
-                            date={"16-03-2019"}
-                            likes={"4"} />
-                    </div>
-
-                </div>
-
-            </div>
-        )
-    }
+        <div style={gridView}>
+          <SideList />
+          <div style={listview}>
+            <ListItem
+              title={"Undefined is not an object React Native"}
+              user={"Hanen Wahabi"}
+              date={"16-03-2019"}
+              likes={"4"}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const container = {
-    backgroundColor: blue,
-}
+  backgroundColor: blue,
+};
 const header = {
-    display: 'flex',
-    flexDirection: 'row',
-    background: 'linear-gradient(to top, #00AB90 0%, #080A38 15%, #080A38 100%)',
-    height: 85,
-    alignItems: 'center'
-}
+  display: "flex",
+  flexDirection: "row",
+  background: "linear-gradient(to top, #00AB90 0%, #080A38 15%, #080A38 100%)",
+  height: 85,
+  alignItems: "center",
+};
 
 const avatar = {
-    height: 35,
-    width: 35,
-    borderRadius: 40,
-    border: '30px solid #2FE090',
-    borderWidth: 2,
-    resizeMode: "cover",
-    marginRight: 40,
-    marginLeft: 20
-}
+  height: 35,
+  width: 35,
+  borderRadius: 40,
+  border: "30px solid #2FE090",
+  borderWidth: 2,
+  resizeMode: "cover",
+  marginRight: 40,
+  marginLeft: 20,
+};
 const listview = {
-    display: 'flex',
-    flex: 3,
-    flexDirection: 'column',
-}
+  display: "flex",
+  flex: 3,
+  flexDirection: "column",
+};
 const gridView = {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 40,
-
-}
-
-
-
-
+  display: "flex",
+  flex: 1,
+  flexDirection: "row",
+  marginTop: 40,
+};
