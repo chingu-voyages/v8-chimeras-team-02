2a9528bf4eb8b784e4db6da3e56436e9ff22f5ec
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-25 10:00:20 +0100

    add giveanswer page temp styles

--- frontend/src/screens/GiveAnswer.js
+++ frontend/src/screens/GiveAnswer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import Home from "./Home";
-import ListItem from "../components/ListItem";
+import { user } from "../resources/images";
+import { blue, green } from "../resources/colors";
+import { SearchBar, Section, ListItem, Logo, SideList } from "../components";
 
 export default class GiveAnswer extends Component {
   state = {
@@ -16,21 +17,88 @@ export default class GiveAnswer extends Component {
 
   render() {
     return (
-      <div>
-        <Home />
-        {this.state.answers.map(answer => (
-          <ListItem
-            title={answer}
-            user={"TheAnswerGiver"}
-            date={"Today"}
-            likes={"100"}
-          />
-        ))}
-        <form onSubmit={this.submitAnswer}>
-          <textarea placeholder="Enter answer..." />
-          <button type="submit">Answer</button>
-        </form>
+      <div style={container}>
+        <div style={header}>
+          <Logo />
+          <SearchBar />
+          <Section title="Ask" />
+          <Section title="Login" />
+          <Section title="Signup" />
+
+          <img src={user} style={avatar} alt={user} />
+        </div>
+
+        <div style={gridView}>
+          <SideList />
+          <div style={listview}>
+            {this.state.answers.map(answer => (
+              <ListItem
+                title={answer}
+                user={"TheAnswerGiver"}
+                date={"Today"}
+                likes={"100"}
+              />
+            ))}
+            <form style={{ display: "flex" }} onSubmit={this.submitAnswer}>
+              <textarea style={textareaStyle} placeholder="Enter answer..." />
+              <button style={btn} type="submit">
+                Answer
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const container = {
+  backgroundColor: blue,
+};
+
+const header = {
+  display: "flex",
+  flexDirection: "row",
+  background: "linear-gradient(to top, #00AB90 0%, #080A38 15%, #080A38 100%)",
+  height: 85,
+  alignItems: "center",
+};
+
+const avatar = {
+  height: 35,
+  width: 35,
+  borderRadius: 40,
+  border: "30px solid #2FE090",
+  borderWidth: 2,
+  resizeMode: "cover",
+  marginRight: 40,
+  marginLeft: 20,
+};
+
+const listview = {
+  display: "flex",
+  flex: 3,
+  flexDirection: "column",
+};
+
+const gridView = {
+  display: "flex",
+  flex: 1,
+  flexDirection: "row",
+  marginTop: 40,
+};
+
+const textareaStyle = {
+  width: "50vw",
+};
+
+const btn = {
+  backgroundColor: green,
+  width: 100,
+  height: 36,
+  alignItems: "center",
+  fontSize: 14,
+  color: "white",
+  fontFamily: "Poppins",
+  border: "0px",
+};
