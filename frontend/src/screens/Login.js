62cc415def583efc20d10a17ab7646c65230a294
joanaammoura <joanaa.mmoura@gmail.com>
2019-03-26 23:44:02 +0000

    Worked on Login Screen

---
+++ frontend/src/screens/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { green } from "../resources/colors";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <form style={container}>
+                <input style={inputs}
+                    type="email"
+                    placeholder="E-mail"
+                    autoComplete="off"
+                    autoFocus="on"
+                // onChange={this.handleChange}
+                // value={this.props.value}
+                ></input>
+                <input style={inputs}
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="off"
+                    autoFocus="on"
+                // onChange={this.props.handleChange}
+                // value={this.props.value}
+                ></input>
+                <input style={button}
+                    type="submit"
+                    value="Log In"
+                ></input>
+            </form>
+        );
+    }
+}
+
+const container = {
+    backgroundColor: "white"
+};
+
+const inputs = {
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid green",
+    background: "white",
+    height: 85,
+    alignItems: "left",
+};
+
+const button = {
+    display: "flex",
+    flexDirection: "row",
+    background: "green",
+    height: 85,
+    alignItems: "center",
+};
