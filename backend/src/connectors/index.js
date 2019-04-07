d3707dbe65217b9c7a41283065e274fdf8798f7b
Hanen Ouahabi <hanenouahabii@gmail.com>
2019-04-07 13:08:26 +0100

    fixing a bug with creating a new question

--- backend/src/connectors/index.js
+++ backend/src/connectors/index.js
@@ -1,207 +1,180 @@
 import uuidv4 from 'uuid/v4';
 const bcrypt = require('bcryptjs');
-const jsonwebtoken = require('jsonwebtoken')
+const jsonwebtoken = require('jsonwebtoken');
 
 export async function Register(name, email, password, mongo) {
-
-    const Users = mongo.collection('User');
-    const existingUser = await Users.findOne({ email });
-    if (existingUser) {
-        throw new Error('Email is already used');
-    }
-    let _id = uuidv4();
-    const token = jsonwebtoken.sign({ userId: _id }, process.env.JWT_SECRET)
-    const hash = await bcrypt.hash(password, 10);
-    await Users.insertOne({
-        email,
-        _id,
-        name,
-        password: hash,
-        rememberToken: token
-    });
-    return await Users.findOne({ email });
+	const Users = mongo.collection('User');
+	const existingUser = await Users.findOne({ email });
+	if (existingUser) {
+		throw new Error('Email is already used');
+	}
+	let _id = uuidv4();
+	const token = jsonwebtoken.sign({ userId: _id }, process.env.JWT_SECRET);
+	const hash = await bcrypt.hash(password, 10);
+	await Users.insertOne({
+		email,
+		_id,
+		name,
+		password: hash,
+		rememberToken: token,
+	});
+	return await Users.findOne({ email });
 }
 
 export async function Login(email, password, mongo) {
-    const Users = mongo.collection('User');
-    const user = await Users.findOne({ $or: [{ email: email }, { phone: email }, { username: email }] });
-    const validPassword = await bcrypt.compare(password, user.password);
+	const Users = mongo.collection('User');
+	const user = await Users.findOne({ $or: [{ email: email }, { phone: email }, { username: email }] });
+	const validPassword = await bcrypt.compare(password, user.password);
 
-    if (!user) {
-        throw new Error('Email not found');
-    }
-    if (!validPassword) {
-        throw new Error('Password is incorrect');
-    }
+	if (!user) {
+		throw new Error('Email not found');
+	}
+	if (!validPassword) {
+		throw new Error('Password is incorrect');
+	}
 
-    const token = jsonwebtoken.sign({ userId: user._id }, process.env.JWT_SECRET)
-    await Users.update({ "_id": user._id }, { $set: { "rememberToken": token } });
+	const token = jsonwebtoken.sign({ userId: user._id }, process.env.JWT_SECRET);
+	await Users.update({ _id: user._id }, { $set: { rememberToken: token } });
 
-    return await Users.findOne({ $or: [{ email: email }, { username: email }] });
+	return await Users.findOne({ $or: [{ email: email }, { username: email }] });
 }
 
 export async function LogOut(mongo, user) {
-    if (!user) {
-        throw new Error("User isn't authenticated");
-    }
-    const Users = mongo.collection('User');
-    let id = user._id;
-    await Users.update({ "_id": id }, { $set: { "rememberToken": "" } });
-    return await Users.findOne({ _id: id });
+	if (!user) {
+		throw new Error("User isn't authenticated");
+	}
+	const Users = mongo.collection('User');
+	let id = user._id;
+	await Users.update({ _id: id }, { $set: { rememberToken: '' } });
+	return await Users.findOne({ _id: id });
 }
 
+export async function createQuestion(title, question, user_id, tags, answers, mongo) {
+	const Questions = mongo.collection('Question');
+	const Users = mongo.collection('User');
+	const user = await Users.findOne({ _id: user_id });
+	let _id = uuidv4();
+	let date = new Date().toISOString();
 
-export async function createQuestion(
-    title,
-    question,
-    user,
-    tags,
-    answers,
-    mongo) {
+	await Questions.insertOne({ _id, title, question, createAt: date, updatedAt: date, user, tags, answers });
 
-    const Questions = mongo.collection('Question');
-    let _id = uuidv4()
-    let date = new Date().toISOString();
-    await Questions.insertOne({ _id, title, question, createAt: date, updatedAt: date, user, tags, answers });
-
-    return await Questions.findOne({ _id });
+	return await Questions.findOne({ _id });
 }
 
 export async function deleteQuestion(_id, mongo) {
-    const Questions = mongo.collection('Question');
-    const existedQuestion = await Questions.findOne({ _id });
+	const Questions = mongo.collection('Question');
+	const existedQuestion = await Questions.findOne({ _id });
 
-    if (!existedQuestion) {
-        throw new Error('This question does not exist');
-    }
-    await Questions.remove({ _id });
-    return existedQuestion;
+	if (!existedQuestion) {
+		throw new Error('This question does not exist');
+	}
+	await Questions.remove({ _id });
+	return existedQuestion;
 }
 
+export async function updateQuestion(_id, title, question, createAt, user_id, tags, answers, mongo) {
+	const Questions = mongo.collection('Question');
+	let updatedAt = new Date().toISOString();
+	const Users = mongo.collection('User');
+	const user = await Users.findOne({ _id: user_id });
 
-export async function updateQuestion(
-    _id,
-    title,
-    question,
-    createAt,
-    user,
-    tags,
-    answers,
-    mongo) {
-    const Questions = mongo.collection('Question');
-    let updatedAt = new Date().toISOString();
+	await Questions.update({ _id }, { title, question, createAt, updatedAt, user, tags, answers });
 
-    await Questions.update({ _id }, { title, question, createAt, updatedAt, user, tags, answers });
-
-    return await Questions.findOne({ _id });
+	return await Questions.findOne({ _id });
 }
-
 
 export async function getQuestion(_id, mongo) {
-    const Questions = mongo.collection('Question');
-    const question = await Questions.findOne({ _id });
+	const Questions = mongo.collection('Question');
+	const question = await Questions.findOne({ _id });
 
-    if (!question) {
-        throw new Error('Not found');
-    }
-    return question;
+	if (!question) {
+		throw new Error('Not found');
+	}
+	return question;
 }
 
+export async function createAnswer(question_id, newAnswer, user, mongo) {
+	// Find the question we're answering
+	const Questions = mongo.collection('Question');
+	const question = await Questions.findOne({ _id: question_id });
 
-export async function createAnswer(
-    question_id,
-    newAnswer,
-    user,
-    mongo) {
+	if (!question) {
+		throw new Error(
+			`Something Wrong! Trying to create answer for a question that doesn't exist. Id: ${question_id}`
+		);
+	}
 
-    // Find the question we're answering
-    const Questions = mongo.collection('Question');
-    const question = await Questions.findOne({ _id: question_id });
+	// Creatre MetaData
+	let _id = uuidv4();
+	let date = new Date().toISOString();
 
+	// Create and push Answer
+	const answer = { _id, answer: newAnswer, createDate: date, updateDate: date, user };
+	question.answers.push(answer);
 
-    if (!question) {
-        throw new Error(`Something Wrong! Trying to create answer for a question that doesn't exist. Id: ${question_id}`);
-    }
+	await Questions.update({ _id }, { answers: question.answers });
 
-    // Creatre MetaData
-    let _id = uuidv4()
-    let date = new Date().toISOString();
-
-    // Create and push Answer
-    const answer = { _id, answer: newAnswer, createDate: date, updateDate: date, user }
-    question.answers.push(answer);
-    
-    await Questions.update( {_id}, { answers: question.answers})
-    
-    return answer;
+	return answer;
 }
-
 
 export async function getAnswer(question_id, _id, mongo) {
-    let answer = "";
+	let answer = '';
 
-    // Get the Question
-    const Questions = mongo.collection('Question');
-    const question = await Questions.findOne({  _id: question_id });
+	// Get the Question
+	const Questions = mongo.collection('Question');
+	const question = await Questions.findOne({ _id: question_id });
 
+	if (!question) {
+		throw new Error(`Something Wrong! Trying to get answer for a question that doesn't exist. Id: ${question_id}`);
+	}
 
-    if (!question) {
-        throw new Error(`Something Wrong! Trying to get answer for a question that doesn't exist. Id: ${question_id}`);
-    }
+	// Loop trough array to check for the _id
+	for (let qstn in question.answers) {
+		if (qstn._id === _id) {
+			answer = qstn;
+		}
+	}
 
-    // Loop trough array to check for the _id
-    for (let qstn in question.answers ) {
-        if (qstn._id === _id){
-            answer = qstn;
-        }
-    }
+	if (!question) {
+		throw new Error(`No Answer found for id: ${question_id}`);
+	}
 
-    if (!question) {
-        throw new Error(`No Answer found for id: ${question_id}`);
-    }
-    
-    return answer;
+	return answer;
 }
 
+export async function updateAnswer(question_id, _id, newAnswer, mongo) {
+	// Get Question
+	const Questions = mongo.collection('Question');
+	const question = await Questions.findOne({ _id: question_id });
 
-export async function updateAnswer(
-    question_id,
-    _id,
-    newAnswer,
-    mongo) {
+	let answer;
 
-    // Get Question
-    const Questions = mongo.collection('Question');
-    const question = await Questions.findOne({  _id: question_id });
+	// Create MetaData
+	let date = new Date().toISOString();
 
-    let answer;
+	// Loop trough array to check for the _id and update answer
+	for (let qstn in question.answers) {
+		if (qstn._id === _id) {
+			qstn.answer = newAnswer;
+			answer = qstn;
+		}
+	}
 
-    // Create MetaData
-    let date = new Date().toISOString();
+	await Questions.update({ _id }, { answers: question.answers });
 
-
-    // Loop trough array to check for the _id and update answer
-    for (let qstn in question.answers ) {
-        if (qstn._id === _id){
-            qstn.answer = newAnswer;
-            answer = qstn;
-        }
-    }
-
-    await Questions.update( {_id}, { answers: question.answers})
-
-    return answer;
+	return answer;
 }
 
+export async function deleteAnswer(question_id, _id, mongo) {
+	// Get Question
+	const Questions = mongo.collection('Question');
+	const question = await Questions.findOne({ _id: question_id });
 
-export async function deleteAnswer( question_id, _id, mongo ) {
-    // Get Question
-    const Questions = mongo.collection('Question');
-    const question = await Questions.findOne({  _id: question_id });
+	filteredAnswers = question.answers.filter(x => {
+		x._id !== _id;
+	});
 
-    filteredAnswers = question.answers.filter( x => { x._id !== _id })
+	await Questions.update({ _id }, { answers: filteredAnswers });
 
-    await Questions.update( {_id}, { answers: filteredAnswers})
-    
-    return answer;
+	return answer;
 }
--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,66 +1,58 @@
 import {
-  Register,
-  Login,
-  LogOut,
-  createQuestion,
-  deleteQuestion,
-  updateQuestion,
-  getQuestion,
-  createAnswer,
-  deleteAnswer,
-  updateAnswer,
-  getAnswer,
+	Register,
+	Login,
+	LogOut,
+	createQuestion,
+	deleteQuestion,
+	updateQuestion,
+	getQuestion,
+	createAnswer,
+	deleteAnswer,
+	updateAnswer,
+	getAnswer,
 } from '../connectors';
 
 const resolvers = {
-  Query: {
-    question: async (root, { _id }, { mongo }, info) => {
-      return getQuestion(_id, mongo);
-    },
-    answer: async (root, { question_id, _id }, { mongo }, info) => {
-      return getAnswer(question_id, _id, mongo);
-    },
-    logOut: async (root, args, { mongo, user }, info) => {
-      return LogOut(mongo, user);
-    },
-    currentUser: async (root, args, { mongo, user }, info) => {
-      return user;
-    },
-  },
-  Mutation: {
-    signup: async (root, { name, email, password }, { mongo }, info) => {
-      return Register(name, email, password, mongo);
-    },
-    login: async (root, { email, password }, { mongo }) => {
-      return Login(email, password, mongo)
-    },
-    createQuestion: async (root,
-      { title, question, user, tags, answers },
-      { mongo }, info) => {
-      return createQuestion(title, question, user, tags, answers, mongo)
-    },
-    deleteQuestion: async (root, { _id }, { mongo }, info) => {
-      return deleteQuestion(_id, mongo);
-    },
-    updateQuestion: async (root,
-      { _id, title, question, createAt, user, tags, answers },
-      { mongo }, info) => {
-      return updateQuestion(_id, title, question, createAt, user, tags, answers, mongo)
-    },
-    createAnswer: async (root,
-      { question_id, answer, user },
-      { mongo }, info) => {
-      return createAnswer(question_id, answer, user, mongo)
-    },
-    deleteAnswer: async (root, { question_id, _id }, { mongo }, info) => {
-      return deleteAnswer(question_id, _id, mongo);
-    },
-    updateAnswer: async (root,
-      { question_id, _id, newAnswer },
-      { mongo }, info) => {
-      return updateAnswer(question_id, _id, newAnswer, mongo)
-    },
-  },
-}
+	Query: {
+		question: async (root, { _id }, { mongo }, info) => {
+			return getQuestion(_id, mongo);
+		},
+		answer: async (root, { question_id, _id }, { mongo }, info) => {
+			return getAnswer(question_id, _id, mongo);
+		},
+		logOut: async (root, args, { mongo, user }, info) => {
+			return LogOut(mongo, user);
+		},
+		currentUser: async (root, args, { mongo, user }, info) => {
+			return user;
+		},
+	},
+	Mutation: {
+		signup: async (root, { name, email, password }, { mongo }, info) => {
+			return Register(name, email, password, mongo);
+		},
+		login: async (root, { email, password }, { mongo }) => {
+			return Login(email, password, mongo);
+		},
+		createQuestion: async (root, { title, question, user_id, tags, answers }, { mongo }, info) => {
+			return createQuestion(title, question, user_id, tags, answers, mongo);
+		},
+		deleteQuestion: async (root, { _id }, { mongo }, info) => {
+			return deleteQuestion(_id, mongo);
+		},
+		updateQuestion: async (root, { _id, title, question, createAt, user, tags, answers }, { mongo }, info) => {
+			return updateQuestion(_id, title, question, createAt, user, tags, answers, mongo);
+		},
+		createAnswer: async (root, { question_id, answer, user }, { mongo }, info) => {
+			return createAnswer(question_id, answer, user, mongo);
+		},
+		deleteAnswer: async (root, { question_id, _id }, { mongo }, info) => {
+			return deleteAnswer(question_id, _id, mongo);
+		},
+		updateAnswer: async (root, { question_id, _id, newAnswer }, { mongo }, info) => {
+			return updateAnswer(question_id, _id, newAnswer, mongo);
+		},
+	},
+};
 
 export default resolvers;
