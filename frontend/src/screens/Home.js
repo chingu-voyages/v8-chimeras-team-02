e691d2b80c097787149ac0d129960b647d8b0668
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-04-09 10:20:58 +0200

    add getQuestion query boiler on Home

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,84 +1,104 @@
 import React, { Component } from 'react';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
 import { blue, green } from '../resources/colors';
-import { SearchBar, Section, ListItem, Logo, SideList, Header, Footer } from '../components';
+import {
+  SearchBar,
+  Section,
+  ListItem,
+  Logo,
+  SideList,
+  Header,
+  Footer,
+} from '../components';
 
 class Home extends Component {
-	render() {
-		return (
-			<div>
-				<Header />
+  render() {
+    return (
+      <div>
+        <Header />
 
-				<div style={gridView}>
-					<SideList />
-					<div style={listview}>
-						<h2 style={{ textAlign: 'left' }}>Top Questions</h2>
-						<ListItem
-							title={'Undefined is not an object React Native'}
-							user={'Hanen Wahabi'}
-							date={'16-03-2019'}
-							likes={'4'}
-						/>
-					</div>
-				</div>
+        <div style={gridView}>
+          <SideList />
+          <div style={listview}>
+            <h2 style={{ textAlign: 'left' }}>Top Questions</h2>
+            <ListItem
+              title={'Undefined is not an object React Native'}
+              user={'Hanen Wahabi'}
+              date={'16-03-2019'}
+              likes={'4'}
+            />
+          </div>
+        </div>
 
-				<div>
-					<Footer />
-				</div>
-			</div>
-		);
-	}
+        <div>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home;
+const GET_QUESTION = gql`
+  {
+    questions {
+      _id
+      title
+      createAt
+    }
+  }
+`;
+
+export default graphql(GET_QUESTION)(Home);
 
 const listview = {
-	display: 'flex',
-	flex: 3,
-	flexDirection: 'column',
-	maxWidth: '50%',
+  display: 'flex',
+  flex: 3,
+  flexDirection: 'column',
+  maxWidth: '50%',
 };
 const gridView = {
-	display: 'flex',
-	flex: 1,
-	flexDirection: 'row',
-	marginTop: 40,
+  display: 'flex',
+  flex: 1,
+  flexDirection: 'row',
+  marginTop: 40,
 };
 
 const form = {
-	display: 'flex',
-	flexDirection: 'column',
-	backgroundColor: 'white',
-	alignItems: 'center',
-	justifyContent: 'center',
-	width: '30%',
-	height: 400,
-	borderRadius: 10,
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: 'white',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '30%',
+  height: 400,
+  borderRadius: 10,
 };
 
 const inputs = {
-	border: '1px solid #ccc',
-	background: 'white',
-	height: 45,
-	width: '40%',
-	alignItems: 'left',
-	marginTop: 25,
-	fontSize: 16,
-	outlineColor: green,
+  border: '1px solid #ccc',
+  background: 'white',
+  height: 45,
+  width: '40%',
+  alignItems: 'left',
+  marginTop: 25,
+  fontSize: 16,
+  outlineColor: green,
 };
 
 const button = {
-	background: green,
-	color: 'white',
-	height: 60,
-	width: '40%',
-	alignItems: 'center',
-	marginTop: 25,
-	fontSize: 20,
+  background: green,
+  color: 'white',
+  height: 60,
+  width: '40%',
+  alignItems: 'center',
+  marginTop: 25,
+  fontSize: 20,
 };
 
 const error = {
-	color: 'red',
-	fontSize: 14,
-	marginTop: 25,
-	textAlign: 'center',
+  color: 'red',
+  fontSize: 14,
+  marginTop: 25,
+  textAlign: 'center',
 };
