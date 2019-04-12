aa8a4d41e8e9b7432296c969a5872efd7ce5c3f2
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-04-12 10:51:47 +0200

    fix fSize typo and prettier clash

--- frontend/src/components/Section.js
+++ frontend/src/components/Section.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
-import { green, blue } from "../resources/colors";
+import { green, blue } from '../resources/colors';
 import styled from 'styled-components';
 
 export default class Section extends Component {
-    state = {
-        btnStyle: <Btn />
-    }
-    render() {
-        return (
-            <div>
-                <button style={this.state.btnStyle}
-                    onClick={this.props.onClick}
-                    onMouseEnter={() => this.setState({ btnStyle: <BtnHover /> })}
-                    onMouseLeave={() => this.setState({ btnStyle: <Btn /> })}>
-                    {this.props.title}
-                </button>
-            </div>
-        )
-    }
+  state = {
+    btnStyle: <Btn />,
+  };
+  render() {
+    return (
+      <div>
+        <button
+          style={this.state.btnStyle}
+          onClick={this.props.onClick}
+          onMouseEnter={() => this.setState({ btnStyle: <BtnHover /> })}
+          onMouseLeave={() => this.setState({ btnStyle: <Btn /> })}
+        >
+          {this.props.title}
+        </button>
+      </div>
+    );
+  }
 }
 
 const Btn = styled.button`
-    fontSize: 16px;
-    color: white;
-    background-color: ${blue};
-    border: 0px;
-    font-family: Poppins;
-    padding: 15px;
+  font-size: 16px;
+  color: white;
+  background-color: ${blue};
+  border: 0px;
+  font-family: Poppins;
+  padding: 15px;
 `;
 
 const BtnHover = styled.button`
-    font-size: 20px;
-    color: ${green};
-    background-color: ${blue};
-    border: 0px;
-    font-family: Poppins;
-    padding: 15px;
+  font-size: 20px;
+  color: ${green};
+  background-color: ${blue};
+  border: 0px;
+  font-family: Poppins;
+  padding: 15px;
 `;
