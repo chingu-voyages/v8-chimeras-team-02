49f5e1040ff7e5fe1e0619c997a2597e502dae6c
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-04-29 10:21:49 +0200

    format date on CompleteItem component

--- frontend/src/components/CompleteItem.js
+++ frontend/src/components/CompleteItem.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { green, purple } from "../resources/colors";
-import { user } from "../resources/images";
+import { green, purple } from '../resources/colors';
+import { user } from '../resources/images';
 import styled from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faHeart);
-
 
 export default class CompleteItem extends Component {
   render() {
@@ -17,97 +16,103 @@ export default class CompleteItem extends Component {
 
         <Body>{this.props.question}</Body>
 
-        <hr style={{width:'100%', marginRight: '40px', color: '#7f7f7f'}} />
+        <hr style={{ width: '100%', marginRight: '40px', color: '#7f7f7f' }} />
 
-              <Item>
-                    <Avatar src={user} alt={user} />
+        <Item>
+          <Avatar src={user} alt={user} />
 
-                    <div style={{ marginLeft: 15, width:'70%' }}>
-                        <Username>{this.props.user}</Username>
-                        <Date>{"published on " + this.props.date}</Date>
-                    </div>
+          <div style={{ marginLeft: 15, width: '70%' }}>
+            <Username>{this.props.user}</Username>
+            <Date>
+              {'published on ' +
+                this.props.date
+                  .split('')
+                  .slice(0, 10)
+                  .join('')}
+            </Date>
+          </div>
 
-                    <div style={{ right: 0, width: '30%', textAlign:'right' }}>
-                      <HeartFill>
-                        <FontAwesomeIcon
-                          icon="heart"
-                          style={{ fontSize: '25px', stroke: `${purple}`, strokeWidth: 10 }}
-                        />
-                      </HeartFill>
-                        <Likes>{this.props.likes + ' likes '}</Likes>
-                    </div>
-                </Item>
-            </CompleteItemContainer>
-        )
-    }
+          <div style={{ right: 0, width: '30%', textAlign: 'right' }}>
+            <HeartFill>
+              <FontAwesomeIcon
+                icon="heart"
+                style={{ fontSize: '25px', stroke: `${purple}`, strokeWidth: 10 }}
+              />
+            </HeartFill>
+            <Likes>{this.props.likes + ' likes '}</Likes>
+          </div>
+        </Item>
+      </CompleteItemContainer>
+    );
+  }
 }
 
 const CompleteItemContainer = styled.div`
-   display: flex;
-   flex-direction: column;
-   width: 100%;
-   box-shadow: 0px 0px 8px 4px gainsboro;
-   padding: 0 20px 10px 20px;
-   margin-bottom: 20px;
-`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0px 0px 8px 4px gainsboro;
+  padding: 0 20px 10px 20px;
+  margin-bottom: 20px;
+`;
 
 const Item = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 80px;
-    width: 100%;
-    margin-top: -15px;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: row;
+  height: 80px;
+  width: 100%;
+  margin-top: -15px;
+  align-items: center;
+`;
 
 const Likes = styled.p`
-    font-size: 12px;
-    color: ${purple};
-    margin-top: 0;
-    margin-bottom: 15px;
-`
+  font-size: 12px;
+  color: ${purple};
+  margin-top: 0;
+  margin-bottom: 15px;
+`;
 
 const Title = styled.p`
-    display: flex;
-    font-size: 20px;
-    color: #000;
-    justify-content: flex-start;
-    margin-bottom: 0;
-`
+  display: flex;
+  font-size: 20px;
+  color: #000;
+  justify-content: flex-start;
+  margin-bottom: 0;
+`;
 
 const Body = styled.p`
-    display: flex;
-    font-size: 14px;
-    color: #7f7f7f;
-    justify-content: flex-start;
-    padding-top: 15px;
-    margin-bottom: 20px;
-`
+  display: flex;
+  font-size: 14px;
+  color: #7f7f7f;
+  justify-content: flex-start;
+  padding-top: 15px;
+  margin-bottom: 20px;
+`;
 
 const Date = styled.p`
-    font-size: 12px;
-    color: #7f7f7f;
-    margin-top: 0;
-    margin-bottom: 0;
-    padding-right: 5px;
-`
+  font-size: 12px;
+  color: #7f7f7f;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-right: 5px;
+`;
 
 const Username = styled.p`
-    font-size: 12px;
-    color: #000;
-    text-align: left;
-    margin: 5px 0;
-`
+  font-size: 12px;
+  color: #000;
+  text-align: left;
+  margin: 5px 0;
+`;
 
 const Avatar = styled.img`
-    height: 30px;
-    width: 30px;
-    border-radius: 40px;
-    border: 30px solid;
-    border-color: ${green};
-    border-width: 6px;
-    background-color: ${green};
-`
+  height: 30px;
+  width: 30px;
+  border-radius: 40px;
+  border: 30px solid;
+  border-color: ${green};
+  border-width: 6px;
+  background-color: ${green};
+`;
 
 const HeartFill = styled.div`
   color: white;
@@ -116,4 +121,4 @@ const HeartFill = styled.div`
   &:hover {
     color: ${purple};
   }
-`
+`;
